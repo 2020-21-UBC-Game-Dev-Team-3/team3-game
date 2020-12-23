c2845e071fb4cb9e7671ebc229205d16c6df71e3
raymond <raymond.1zou@gmail.com>
2020-12-22 17:28:33 -0800

    Kill mechanic
    - Kill mechanic (will have to manually assign the target atm)
    - Assassin class

---
+++ Multiplayer Bullshit/Assets/Scripts/Player Stuff/Assassin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class Assassin : MonoBehaviour {
+
+  private string ability1 = "e";
+  private string nameOfWeapon = "Knife";
+
+  private float headLevelHeight = 1;
+
+  [SerializeField] GameObject myTarget;
+
+  private void Update() {
+    if (Input.GetKeyDown(ability1) && IsMyController()) { // w/o IsMyController() pressing e would instantiate something for ALL players in the game
+      ThrowKnife();
+    }
+  }
+
+  public bool IsMyController() {
+    PhotonView pv = GetComponent<PhotonView>();
+    return pv.IsMine;
+  }
+
+  private void ThrowKnife() {
+    if (myTarget == null) {
+      Debug.Log("No valid target selected!");
+    } else if (myTarget.tag == "Player") {
+      Vector3 offset = transform.forward;
+      Vector3 headLevel = new Vector3(transform.position.x, transform.position.y + headLevelHeight, transform.position.z);
+      PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", nameOfWeapon), headLevel + offset, Quaternion.identity);
+      Debug.Log("knife released");
+    }
+  }
+
+  public Vector3 GetTargetPosition() {
+    if (myTarget != null) {
+      Debug.Log(myTarget.transform.position);
+      return myTarget.transform.position;
+    } else {
+      Debug.Log("ERROR: assassin has no target"); //replace this with throwing an exception?
+      return new Vector3(0, 0, 0);
+    }
+  }
+}
---
+++ Multiplayer Bullshit_clone_0/Assets/Scripts/PhotonLauncher.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class PhotonLauncher : MonoBehaviourPunCallbacks
+{
+    public static PhotonLauncher Instance;
+    
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+
+    [SerializeField] Transform roomListContent;
+    [SerializeField] Transform playerListContent;
+    [SerializeField] GameObject roomListPrefab;
+    [SerializeField] GameObject playerListPrefab;
+
+    [SerializeField] GameObject startGameButton;
+
+    void Awake()
+    {
+        Instance = this; 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting to master.");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Joined master.");
+        PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("title");
+        Debug.Log("Joined lobby.");
+        PhotonNetwork.NickName = "Player " + Random.Range(0, 1000).ToString("0000");
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text)) return;
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        foreach(Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Instantiate(playerListPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+        }
+
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room creation failed: " + message;
+        MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name); 
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("title");
+
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(Transform transform in roomListContent)
+        {
+            Destroy(transform.gameObject);
+        }
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            if (roomList[i].RemovedFromList) continue;
+            Instantiate(roomListPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(playerListPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
+    }
+
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+}
