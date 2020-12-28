c6e2cde1cf58b3c35ad085190c5d8b26f7836770
Nafis Forkan <forkannafis@gmail.com>
2020-12-28 13:15:43 -0800

    First Commit
    Scene Layout
    Mouse Dragging
    LifeBoat Movement Framework
    
    Work needs to be done to combine game aspects to complete game.
    Assets for game objects needs to be created by Creative team.
    Background Image
    Passenger Sprites
    Lifeboat Sprite
    Partial Cruise Ship Sprite

---
+++ Multiplayer Bullshit/Assets/LifeBoatMinigame/LifeBoatGameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBoatGameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject LifeBoat;
+    public GameObject Passenger1;
+    
+
+    private int score;
+    public Text ScoreText;
+
+    [SerializeField] int threshold;
+
+    private bool isSafe;
+
+    public bool isWin;
+
+    
+
+
+
+    void OnTrigger ( ){
+
+    }
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
