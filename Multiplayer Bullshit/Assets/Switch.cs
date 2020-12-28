8c580019392e099bb62d5f20c3c0bddeaba51cbf
raymond <raymond.1zou@gmail.com>
2020-12-27 23:03:11 -0800

    Message on light switch click
    - Message will be displayed to console whenever one of the three light switches is clicked on

---
+++ Multiplayer Bullshit/Assets/Switch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+  [SerializeField] private string selectedTag;
+
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+    CheckRay();
+  }
+
+  private void CheckRay() {
+    if (Input.GetMouseButtonDown(0)) {
+      var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit)) {
+        if (hit.collider.tag == selectedTag) {
+          Debug.Log("Switch!");
+        }
+      }
+    }
+  }
+
+}
