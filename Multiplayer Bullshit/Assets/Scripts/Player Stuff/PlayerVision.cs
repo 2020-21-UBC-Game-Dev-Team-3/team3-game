0c833232e71f3a45bee35a1f36853405304c51c4
raymond <raymond.1zou@gmail.com>
2021-01-04 15:18:42 -0800

    Fixes to make game runnable
    - Changed lighting settings to make Gaming scene dark
    - Deleted duplicate Interactable script
    - Moved line of code in PlayerVision where pv is received from Start() to Awake()

--- Multiplayer Bullshit/Assets/Scripts/Player Stuff/PlayerVision.cs
+++ Multiplayer Bullshit/Assets/Scripts/Player Stuff/PlayerVision.cs
@@ -3,41 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class PlayerVision : MonoBehaviour
-{
-    public float visionRadius;
-    private PhotonView pv;
+public class PlayerVision : MonoBehaviour {
+  public float visionRadius;
+  private PhotonView pv;
 
-    //NOTE: LOTS OF DUPLICATION DUE TO UNITY CHAN. ONCE YOU REPLACE OBJECT SOME OF THIS CODE MIGHT NOT WORK PROPERLY
-    //SO ADJUST UNITY CHAN APPROPIATELY
+  //NOTE: LOTS OF DUPLICATION DUE TO UNITY CHAN. ONCE YOU REPLACE OBJECT SOME OF THIS CODE MIGHT NOT WORK PROPERLY
+  //SO ADJUST UNITY CHAN APPROPIATELY
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        pv = GetComponent<PhotonView>();
-        GetComponent<SphereCollider>().radius = visionRadius;
-        if (pv.IsMine) EnableChildren(gameObject);
-    }
+  // Start is called before the first frame update
 
-    void EnableChildren(GameObject c)
-    {
-        for (int i = 0; i < c.transform.childCount; i++)
-            c.transform.GetChild(i).gameObject.SetActive(true);
-    }
+  private void Awake() {
+    pv = GetComponent<PhotonView>();
+  }
 
-    void DisableChildren(GameObject c)
-    {
-        for (int i = 0; i < c.transform.childCount; i++)
-            c.transform.GetChild(i).gameObject.SetActive(false);
-    }
+  void Start() {
+    /*pv = GetComponent<PhotonView>();*/   // this line has been moved to the Awake() method
+    GetComponent<SphereCollider>().radius = visionRadius;
+    if (pv.IsMine) EnableChildren(gameObject);
+  }
 
-    private void OnTriggerEnter(Collider collider)
-    {
-        if (pv.IsMine && collider.gameObject != gameObject && collider.gameObject.tag == "Player") EnableChildren(collider.gameObject);
-    }
+  void EnableChildren(GameObject c) {
+    for (int i = 0; i < c.transform.childCount; i++)
+      c.transform.GetChild(i).gameObject.SetActive(true);
+  }
 
-    private void OnTriggerExit(Collider collider)
-    {
-        if (pv.IsMine && collider.gameObject != gameObject && collider.gameObject.tag == "Player") DisableChildren(collider.gameObject);
-    }
+  void DisableChildren(GameObject c) {
+    for (int i = 0; i < c.transform.childCount; i++)
+      c.transform.GetChild(i).gameObject.SetActive(false);
+  }
+
+  private void OnTriggerEnter(Collider collider) {
+    if (pv.IsMine && collider.gameObject != gameObject && collider.gameObject.tag == "Player") EnableChildren(collider.gameObject);
+  }
+
+  private void OnTriggerExit(Collider collider) {
+    if (pv.IsMine && collider.gameObject != gameObject && collider.gameObject.tag == "Player") DisableChildren(collider.gameObject);
+  }
 }
