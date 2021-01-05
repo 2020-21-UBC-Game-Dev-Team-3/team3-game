02d5c7f0408b34763fc2e7317af3b6c4ec4a7fed
Nafis Forkan <forkannafis@gmail.com>
2021-01-05 02:22:28 -0800

    Die Ghost Body Commit
    Body intsantiate on kill
    Player switches to Ghost Controls
    
    To Do:
    Change the layer of the ghost player
    Get camera to work with that mechanic

---
+++ Multiplayer Bullshit_clone_0/Assets/Scripts/Player Stuff/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView pv;
+
+    void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(pv.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        Vector3 position = new Vector3(0f, 10f, 0f);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "unitychan"), position, Quaternion.identity);
+    }
+
+}
