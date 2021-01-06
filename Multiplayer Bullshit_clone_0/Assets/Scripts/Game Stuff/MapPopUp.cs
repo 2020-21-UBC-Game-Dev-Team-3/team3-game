e36f5e5e1fbcb3069cb825900eea7328b0ebcaee
raymond <raymond.1zou@gmail.com>
2021-01-05 18:47:31 -0800

    Merged Nafis' branch to main
    - Ghost mechanic
    - Dead body instatiation
    - Killing mechanic
    - NOTE: PlayerVision script not a component of unitychan (eg. players will be able to see each other across the map)

---
+++ Multiplayer Bullshit_clone_0/Assets/Scripts/Game Stuff/MapPopUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPopUp : MonoBehaviour
+{
+    [SerializeField] GameObject map;
+    
+    public void Open()
+    {
+        map.SetActive(true);
+    }
+
+    public void Close()
+    {
+        map.SetActive(false);
+    }
+}
