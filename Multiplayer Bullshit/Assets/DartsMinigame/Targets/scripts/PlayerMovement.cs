c3006dd2f553f7e542c90bf571bf83df8672f503
jayp213 <73633196+jayp213@users.noreply.github.com>
2021-02-18 14:25:28 -0800

    Updates
    -Mouse controls player
    -Smooth random movement of targets

---
+++ Multiplayer Bullshit/Assets/DartsMinigame/Targets/scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(50, 50);
+    private Vector3 mousePos = Vector3.zero;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        /*float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);*/
+        mousePos.z = transform.position.z - Camera.main.transform.position.z;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
---
+++ Multiplayer Bullshit/Assets/DartsMinigame/Targets/scripts/RandomNumberGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+
+public class RandomNumberGenerator : MonoBehaviour
+{
+    private TMP_Text m_textComponent;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+        // Text numbers = GameObject.Find("Canvas/Text").GetComponent<Text>();
+        // numbers.text = "1 2 3 4 5";
+        int[] nums = randomNumberGenerator();
+        string word = string.Join(", ", nums.Select(i => i.ToString()).ToArray());
+        Debug.Log(word);
+        m_textComponent = GetComponent<TMP_Text>();
+        m_textComponent.text = word;
+
+
+        
+    }
+
+    private int[] randomNumberGenerator() {
+        int[] nums = new int[] { 1,2,3,4,5 };
+        for(int i = 0; i < nums.Length; i++){
+            int tmp = nums[i];
+            int r = Random.Range(i, nums.Length);
+            nums[i] = nums[r];
+            nums[r] = tmp;
+        }
+
+        return nums;
+
+    }
+
+   
+
+    
+}
