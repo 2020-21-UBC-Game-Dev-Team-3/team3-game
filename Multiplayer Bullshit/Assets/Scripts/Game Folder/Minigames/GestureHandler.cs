89dc8822c5f665e39388d26e6fc70b47db080789
mohan <mohan101@students.cs.ubc.ca>
2021-02-20 18:04:35 -0800

    Finished Drink Minigame

---
+++ Multiplayer Bullshit/Assets/Scripts/Game Folder/Minigames/GestureHandler.cs
@@ -0,0 +1,75 @@
+﻿using GestureRecognizer;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestureHandler : MonoBehaviour
+{
+	private int currentStepNumber = 0;
+	public float drawingAccuracyPercentage;
+
+	public Text textResult;
+	public GameObject winText;
+	public Transform referenceRoot;
+
+	public DrawingManager drawingManager;
+	GesturePatternDraw[] references;
+
+	void Start()
+	{
+		references = referenceRoot.GetComponentsInChildren<GesturePatternDraw>();
+	}
+
+	void ShowAll()
+	{
+		for (int i = 0; i < references.Length; i++)
+		{
+			references[i].gameObject.SetActive(true);
+		}
+	}
+
+	public void OnRecognize(RecognitionResult result)
+	{
+		StopAllCoroutines();
+		ShowAll();
+		if (result != RecognitionResult.Empty)
+		{
+			if (IsCorrectGesture(result))
+			{
+				currentStepNumber++;
+				if (currentStepNumber >= drawingManager.patternReferences.Length)
+					WinGestureMinigame();
+				StartCoroutine(Blink(result.gesture.id));
+			}
+		}
+	}
+
+	bool IsCorrectGesture(RecognitionResult result)
+    {
+		return result.gesture.id == drawingManager.gesturePatterns[currentStepNumber].id && Mathf.RoundToInt(result.score.score * 100) > drawingAccuracyPercentage;
+    }
+
+	void WinGestureMinigame()
+    {
+		winText.SetActive(true);
+    }
+
+	IEnumerator Blink(string id)
+	{
+		GetComponent<DrawDetector>().ClearLines();
+		var draw = references.Where(e => e.pattern.id == id).FirstOrDefault();
+		if (draw != null)
+		{
+			var seconds = new WaitForSeconds(0.1f);
+			for (int i = 0; i <= 20; i++)
+			{
+				draw.gameObject.SetActive(i % 2 == 0);
+				yield return seconds;
+			}
+			draw.gameObject.SetActive(true);
+		}
+		GetComponent<DrawDetector>().ClearLines();
+	}
+}
---
+++ Multiplayer Bullshit/Assets/Scripts/Game Folder/Minigames/GlyphPatternScript.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlyphPatternScript : MonoBehaviour
+{
+    public enum Patterns
+    {
+        Triangle, Square, Pentagon
+    }
+    [HideInInspector] public Vector3[] triangleCorners = new Vector3[4] { new Vector3(0f, 0f), new Vector3(-1f, -1f), new Vector3(1f, -1f), new Vector3(0f, 0f) };
+    [HideInInspector] public Vector3[] squareCorners = new Vector3[5] { new Vector3(1f, 0f), new Vector3(1f, -1f), new Vector3(-1f, -1f), new Vector3(-1f, 0f), new Vector3(1f, 0f) };
+    [HideInInspector] public Vector3[] pentagonCorners = new Vector3[6] { new Vector3(0f, 0f), new Vector3(-1f, -0.5f), new Vector3(-0.5f, -1f), new Vector3(0.5f, -1f), new Vector3(1f, -0.5f), new Vector3(0f, 0f) };
+    private List<Patterns> takenPatterns = new List<Patterns>();
+    public DrawingScript drawingScript;
+    public Transform[] stepTransforms = new Transform[3];
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupGame();
+    }
+
+    void SetupGame()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Patterns pattern = (Patterns)UnityEngine.Random.Range(0, 3);
+            while (takenPatterns.Contains(pattern)) pattern = (Patterns)UnityEngine.Random.Range(0, 3);
+            CheckPattern(stepTransforms[i], pattern);
+            takenPatterns.Add(pattern);
+        }
+
+    }
+
+    void CheckPattern(Transform step, Patterns pattern)
+    {
+        Debug.Log("Checking pattern");
+        switch(pattern)
+        {
+            case Patterns.Triangle: 
+                CreateShape(step, CreateTriangle);
+                break;
+            case Patterns.Square:
+                CreateShape(step, CreateSquare);
+                break;
+            case Patterns.Pentagon:
+                CreateShape(step, CreatePentagon);
+                break;
+        }
+    }
+    public void CreateShape(Transform step, Action<Vector3, LineRenderer> shapeCreator)
+    {
+        GameObject currDrawingBrush = Instantiate(drawingScript.drawingBrushPrefab, step);
+        LineRenderer currLineRenderer = currDrawingBrush.GetComponent<LineRenderer>();
+        Vector3 stepPosition = new Vector3(step.position.x, step.position.y + 0.5f, 0);
+        shapeCreator(stepPosition, currLineRenderer);
+    }
+
+    public void CreateTriangle(Vector3 stepPosition, LineRenderer lineRenderer)
+    {
+        lineRenderer.positionCount = 4;
+        for(int vector = 0; vector < 4; vector++) triangleCorners[vector] = triangleCorners[vector] + stepPosition;
+        lineRenderer.SetPositions(triangleCorners);
+
+    }
+    public void CreateSquare(Vector3 stepPosition, LineRenderer lineRenderer)
+    {
+        lineRenderer.positionCount = 5;
+        for (int vector = 0; vector < 5; vector++) squareCorners[vector] = squareCorners[vector] + stepPosition;
+        lineRenderer.SetPositions(squareCorners);
+    }
+
+    public void CreatePentagon(Vector3 stepPosition, LineRenderer lineRenderer)
+    {
+        lineRenderer.positionCount = 6;
+        for (int vector = 0; vector < 6; vector++) pentagonCorners[vector] = pentagonCorners[vector] + stepPosition;
+        lineRenderer.SetPositions(pentagonCorners);
+    }
+}
