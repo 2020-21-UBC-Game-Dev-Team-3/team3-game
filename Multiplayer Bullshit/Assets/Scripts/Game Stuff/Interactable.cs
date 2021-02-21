b5a5e5c34ee6fb76e1d6a719930318159dfa88d4
raymond <raymond.1zou@gmail.com>
2021-02-20 21:04:29 -0800

    Added task bar & scavenger hunt minigame
    - Press "E" on among us plushie to star the scavenger hunt game
    - Once all 3 items are collected, task bar increments by 1

--- Multiplayer Bullshit/Assets/Scripts/Game Stuff/Interactable.cs
+++ Multiplayer Bullshit/Assets/Scripts/Game Stuff/Interactable.cs
@@ -1,36 +1,34 @@
 ﻿using UnityEngine;
 using Photon.Pun;
 
-public abstract class Interactable : MonoBehaviourPunCallbacks
-{
-    public string interactableName;
+public abstract class Interactable : MonoBehaviourPunCallbacks {
+  public string interactableName;
 
-    public Transform interactableTransform;
+  public Transform interactableTransform;
 
-    [SerializeField] GameObject indicator;
+  //[SerializeField] GameObject indicator;
 
-    Outline outline;
+  [HideInInspector] public Outline outline;
 
-    void Awake() => outline = GetComponent<Outline>();
+  void Awake() {
+    outline = GetComponent<Outline>();
+    outline.enabled = false;
+  }
 
-    public override void OnEnable() => indicator = interactableTransform.GetChild(0).gameObject;
+  //public override void OnEnable() => indicator = interactableTransform.GetChild(0).gameObject;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") && other.gameObject.GetComponent<PlayerActionController>().pv.IsMine)
-        {
-            indicator.SetActive(true);
-            outline.enabled = true;
-        }
+  private void OnTriggerEnter(Collider other) {
+    if (other.CompareTag("Player") && other.gameObject.GetComponent<PlayerActionController>().pv.IsMine) {
+      //indicator.SetActive(true);
+      outline.enabled = true;
     }
+  }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player") && other.gameObject.GetComponent<PlayerActionController>().pv.IsMine)
-        {
-            indicator.SetActive(false);
-            outline.enabled = false;
-        }
+  private void OnTriggerExit(Collider other) {
+    if (other.CompareTag("Player") && other.gameObject.GetComponent<PlayerActionController>().pv.IsMine) {
+      //indicator.SetActive(false);
+      outline.enabled = false;
     }
+  }
 
 }
