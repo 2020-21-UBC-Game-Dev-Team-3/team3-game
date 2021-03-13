47dc87c57affd2a644369850f91ea2f81cafc341
Chiara Lin <chiara.stefanialin@gmail.com>
2021-03-12 19:56:00 -0600

    Debugging

---
+++ Multiplayer Bullshit/Assets/Scripts/UI Stuff/OptionsPP.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+public static class OptionsPP {
+  public static Volume volume;
+
+  public static float bloomValue = 0;
+
+  public static float brightnessValue = 0; // [0, 0.95]
+
+  public static float shadowsValue = 1;
+
+}
---
+++ Multiplayer Bullshit/Assets/Scripts/UI Stuff/Settings.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Settings : MonoBehaviour
+{
+    public Volume volume;
+//    public Slider BloomIntensitySlider;
+    // public Slider LGGSlider;
+    // public float LGGValue;
+    // public Dropdown shadows;
+
+    private void Update(){
+        ChangeBloomIntensitySettings();
+    }
+
+    public void ChangeBloomIntensitySettings()
+    {
+        GameObject gameObject = GameObject.Find("Post Processing");
+        volume = gameObject.GetComponent<Volume>();
+        Bloom bloom;
+        volume.profile.TryGet(out bloom);
+        Debug.Log( bloom.intensity.value);
+        bloom.intensity.value = OptionsPP.bloomValue;
+
+   }
+
+
+    // public void ChangeLggSettings(){
+    //     GameObject gameObject = GameObject.Find("Volume");
+    //     volume = gameObject.GetComponent<Volume>();
+    //     ColorAdjustments ca;
+    //     volume.profile.TryGet(out ca);
+    //     ca.postExposure.value = LGGValue;
+
+    // }
+
+} 
