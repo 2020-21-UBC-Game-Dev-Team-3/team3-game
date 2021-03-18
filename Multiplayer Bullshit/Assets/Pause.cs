7d74d98337773a21dd88d6415a6ff2857f9ad162
Chiara Lin <chiara.stefanialin@gmail.com>
2021-03-17 19:13:32 -0500

    In-game options menu

---
+++ Multiplayer Bullshit/Assets/Pause.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+   public GameObject Canvas;
+    public Camera Camera;
+    bool Paused = false;
+ 
+    void Start(){
+        Canvas.gameObject.SetActive (false);
+     }
+ 
+    void Update () {
+        if (Input.GetKeyDown ("escape")) {
+            if(Paused == true){
+                Time.timeScale = 1.0f;
+                Canvas.gameObject.SetActive (false);
+                Cursor.visible = false;
+                Cursor.lockState =  CursorLockMode.Confined;
+                //Camera.audio.Play ();
+                Paused = false;
+             } else {
+                Time.timeScale = 0.0f;
+                Canvas.gameObject.SetActive (true);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Locked;
+                //Camera.audio.Pause ();
+                Paused = true;
+             }
+         }
+     }
+
+     public void Resume(){
+        Time.timeScale = 1.0f;
+        Canvas.gameObject.SetActive (false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        // Camera.audio.Play ();
+     }
+     public void QuitGame(){
+         Application.Quit();
+     } 
+}
