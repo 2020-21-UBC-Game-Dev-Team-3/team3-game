465fe65bbb6ddb2af210fd9aae12f1917d4617b0
afroad <56143597+afroad@users.noreply.github.com>
2021-03-23 15:47:24 -0400

    light fix 100

--- Multiplayer Bullshit/Assets/Scripts/Minigames Folder/lights sabotage/CrewmateLightsOn.cs
+++ Multiplayer Bullshit/Assets/Scripts/Minigames Folder/lights sabotage/CrewmateLightsOn.cs
@@ -8,47 +8,30 @@ using HutongGames.PlayMaker;
 public class CrewmateLightsOn : MonoBehaviour
 {
     public Volume volume;
-    //public double exposureValue = -1.5;
     public double vignettevalue = 0.8;
-    public Vector4 gainvalue = new Vector4(.3f, .3f, .3f, .3f);
     public double chromaticaberrationvalue = .99;
-    //ColorAdjustments coloradjustments;
     public Vignette vignette;
     public ChromaticAberration chromaticaberration;
-    public LiftGammaGain liftgammagain;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //exposureValue = -1.5;
         vignettevalue = 0.8;
         chromaticaberrationvalue = .99;
-        liftgammagain.gain.value = gainvalue;
-        //volume.profile.TryGet<ColorAdjustments>(out coloradjustments);
         volume.profile.TryGet<Vignette>(out vignette);
         volume.profile.TryGet<ChromaticAberration>(out chromaticaberration);
-        volume.profile.TryGet<LiftGammaGain>(out liftgammagain);
-
     }
 
     void ResetLights2()
     {
-        //exposureValue = -1.5;
-
         vignettevalue = 0.8;
         chromaticaberrationvalue = .99;
-        liftgammagain.gain.value = gainvalue;
     }
 
     void Update()
     {
 
-        liftgammagain.gain.value += new Vector4(0.3f, 0.3f, 0.3f, 0.3f) * Time.deltaTime;
-
-        if (liftgammagain.gain.value.magnitude >= new Vector4(1f, 1f, 1f, 1f).magnitude)
-        {
-            liftgammagain.gain.value = new Vector4(1f, 1f, 1f, 1f);
-        }
 
         chromaticaberration.intensity.value = (float)(chromaticaberrationvalue -= .20 * Time.deltaTime);
 
--- Multiplayer Bullshit/Assets/Scripts/Minigames Folder/lights sabotage/crewmateLightsOut.cs
+++ Multiplayer Bullshit/Assets/Scripts/Minigames Folder/lights sabotage/crewmateLightsOut.cs
@@ -9,48 +9,29 @@ using HutongGames.PlayMaker;
 public class crewmateLightsOut : MonoBehaviour
 {
     public Volume volume;
-    //public double exposureValue = .05;
     public double vignettevalue = 0.2;
-    public Vector4 gainvalue = new Vector4(1,1,1,1);
     public double chromaticaberrationvalue = 0;
-    //ColorAdjustments coloradjustments;
     public Vignette vignette;
     public ChromaticAberration chromaticaberration;
-    public LiftGammaGain liftgammagain;
     public PlayMakerFSM myFSM;
     
    
     // Start is called before the first frame update
     void Start()
     {
-
-        //volume.profile.TryGet<ColorAdjustments>(out coloradjustments);
         volume.profile.TryGet<Vignette>(out vignette);
         volume.profile.TryGet<ChromaticAberration>(out chromaticaberration);
-        volume.profile.TryGet<LiftGammaGain>(out liftgammagain);
-
     }
 
     void ResetLights()
     {
-
-        liftgammagain.gain.value = gainvalue; 
         vignettevalue = 0.2;
         chromaticaberrationvalue = 0;
-
     }
 
 
     void Update()
     {
-
-        liftgammagain.gain.value -= new Vector4(0.1f, 0.1f, 0.1f, 0.1f) * Time.deltaTime;
-
-        if (liftgammagain.gain.value.magnitude <= new Vector4(0.3f, 0.3f, 0.3f, 0.3f).magnitude)
-        {
-            liftgammagain.gain.value = new Vector4(0.3f, 0.3f, 0.3f, 0.3f);
-        }
-
        vignette.intensity.value = (float)(vignettevalue += .20 * Time.deltaTime);
 
         if (vignettevalue >= .8)
