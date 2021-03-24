620af615806edb7f3e9f8634b0c499fd0a73ef6c
Kevin Chong <kevinchong314@gmail.com>
2021-03-23 21:45:27 -1000

    Fixed UI elements and other miscellaneous things
    -Added sprite images in the character select canvas
    -Moved volume slider to in-game settings menu
    -Added photon transform view on shuriken
    -Removed duplicate chameleon ability on player prefab

---
+++ Multiplayer Bullshit/Assets/Scripts/UI Stuff/UISkinSet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISkinSet : MonoBehaviour
+{
+    [SerializeField] List<Sprite> skinImages;
+    [SerializeField] GameObject uiSkinImage;
+
+    public void ChangeActiveUISkinImage(int index)
+    {
+        uiSkinImage.GetComponent<Image>().sprite = skinImages[index];
+    }
+}
